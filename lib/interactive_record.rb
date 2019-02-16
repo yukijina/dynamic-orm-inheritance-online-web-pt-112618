862df6103651fdfdb6f2ef848a29c5d60717ac4d
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-16 18:17:20 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,13 +11,13 @@ class InteractiveRecord
     DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
-
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
       column_names << row["name"]
     end
     column_names.compact
+    binding.pry
   end
 
   def initialize(options={})
